c03e58680ebc531444c39c8464a5ab88943574e1
Ivo Lopes <ivolopes.developer@gmail.com>
2021-10-02 17:37:26 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+### Simple Python Browser
+
+<br />
+
+This is a simple python browser that I made just for fun and experience with QtWebEngineWidgets.
+
+<br />
+<br />
+
+## Some Images
+
+<br/>
+
+![Home Page](images/img1.png)  
+![Youtube Page](images/img2.png)
+
+<br />
+<br />
+<br />
+
+And that's all, thank you 😁
---
+++ main.py
@@ -0,0 +1,66 @@
+# date: 02/10/2021
+# author: Ivo Lopes
+# -------------------------------
+# DEPENDENCIES
+# pip install PyQt5
+# pip install PyQtWebEngine
+
+
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5 import QtGui
+from PyQt5.QtGui import QIcon
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('http://duckduckgo.com/'))
+        self.setCentralWidget(self.browser)
+        self.setWindowIcon(QtGui.QIcon('window-icon.png'))
+        self.showMaximized()
+
+        # navigation bar ----------------
+        navbar = QToolBar()
+        self.addToolBar(navbar)
+
+        back_btn = QAction('Go Back', self)
+        back_btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_btn)
+
+        forward_btn = QAction('Go Forward', self)
+        forward_btn.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_btn)
+
+        reload_btn = QAction('Reload', self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        home_btn = QAction('Home', self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
+
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
+        self.browser.urlChanged.connect(self.update_url)
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl('http://duckduckgo.com/'))
+
+    def navigate_to_url(self):
+        url = "http://" + self.url_bar.text() + "/"
+        self.browser.setUrl(QUrl(url))
+
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
+
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName('Typhon Browser - made with Python')
+window = MainWindow()
+
+app.exec_()
